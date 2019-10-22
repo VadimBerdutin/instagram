8921883fbb88d4877fdc19f8295bae93cea7acf6
vadim <stomrrainingg@gmail.com>
2019-10-22 16:41:47 +0300

    All source files for Lesson 4.

---
+++ tests/test.py
@@ -0,0 +1,27 @@
+import time
+
+from selenium import webdriver
+from pages.login_page import LoginPage
+from pages.main_page import MainPage
+from pages.search_results_page import SearchResultsPage
+
+driver = webdriver.Chrome("D:/Tools/chromedriver.exe")
+driver.implicitly_wait(5)
+
+driver.get("https://www.instagram.com/accounts/login/")
+
+login_page = LoginPage(driver)
+login_page.enter_username("holy_hedgehog___")
+login_page.enter_password("ESqwerty5892")
+login_page.click_login()
+
+main_page = MainPage(driver)
+main_page.click_not_now_button()
+main_page.type_in_search_field("#fitness")
+main_page.click_result_with_text("#fitness")
+time.sleep(3)
+search_results_page = SearchResultsPage(driver)
+# assert "Follow" in search_results_page.get_follow_button_text()
+assert "Follow" in search_results_page.get_button_text()
+
+driver.quit()
